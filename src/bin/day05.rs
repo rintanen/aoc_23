f1995b57d7e7fbcd206c9232005a7102d43d5d14
rintanen <pee.rintanen@gmail.com>
2023-12-05 20:32:46 +0200

    asfasfas

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -4,12 +4,6 @@ struct Map {
 }
 
 impl Map {
-    fn new() -> Self {
-        Self {
-            ranges: Vec::new(),
-        }
-    }
-
     fn find_range(&self, item: u32) -> Option<&Range> {
         self.ranges.iter().find(|range| range.contains(item))
     }
@@ -27,14 +21,6 @@ struct Range {
 }
 
 impl Range {
-    fn new(source_start: u32, destination_start: u32, length: u32) -> Self {
-        Self {
-            source_start,
-            destination_start,
-            length,
-        }
-    }
-
     fn contains(&self, item: u32) -> bool {
         item >= self.source_start && item < self.source_start + self.length
     }
@@ -59,11 +45,80 @@ fn parse_map(section: &str) -> Map {
             let destination_start = line_iter.next().unwrap().parse::<u32>().unwrap();
             let source_start = line_iter.next().unwrap().parse::<u32>().unwrap();
             let length = line_iter.next().unwrap().parse::<u32>().unwrap();
-            Range::new(source_start, destination_start, length)
+            Range {source_start, destination_start, length }
         })
         .collect::<Vec<Range>>();
     Map { ranges }
 }
+
+
+fn map_seed_to_location(seed: u32, maps: &[Map]) -> u32 {
+    let mut item = seed;
+    for map in maps {
+        item = map.map_source_to_destination(item);
+    }
+    item
+}
+
+
+// fn binary_search<F>(mut start: u32, mut end: u32, mut f: F) -> u32
+// where
+//     F: FnMut(u32) -> bool,
+// {
+//     while start < end {
+//         let mid = start + (end - start) / 2;
+//         if f(mid) {
+//             end = mid;
+//         } else {
+//             start = mid + 1;
+//         }
+//     }
+//     start
+// }
+// //
+// //
+// fn ternary_search<F>(mut start: u32, mut end: u32, mut f: F) -> u32
+// where
+//     F: FnMut(u32) -> bool,
+// {
+//     while start < end {
+//         let mid1 = start + (end - start) / 3;
+//         let mid2 = end - (end - start) / 3;
+//         if f(mid1) {
+//             end = mid1;
+//         } else if f(mid2) {
+//             start = mid1 + 1;
+//             end = mid2;
+//         } else {
+//             start = mid2 + 1;
+//         }
+//     }
+//     start
+// }
+
+fn binary_search_minimizer(maps: &[Map], low: u32, high: u32) -> u32 {
+    const EPSILON: u32 = 1;
+
+    let mut low = low;
+    let mut high = high;
+
+    while high - low > EPSILON {
+        let mid = low + (high - low) / 2;
+
+        // Evaluate the function at mid and mid+1
+        let mid_value = map_seed_to_location(mid, maps);
+        let mid_plus_one_value = map_seed_to_location(mid + 1, maps);
+
+        if mid_value <= mid_plus_one_value {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    low
+}
+
 
 
 fn main() {
@@ -75,9 +130,30 @@ fn main() {
     let maps = section_iter
         .map(|section| parse_map(section))
         .collect::<Vec<Map>>();
-    dbg!(maps);
+    // dbg!(&maps);
+
+    let pt1 = seeds
+        .iter()
+        .map(|seed| map_seed_to_location(*seed, &maps))
+        .min()
+        .unwrap();
+
+    println!("Part 1: {:?}", pt1);
 
 
 
+    let seed_ranges = seeds.chunks(2).map(|s| (s[0], s[0] + s[1] - 1)).collect::<Vec<_>>();
+    // let fars = seed_ranges.iter().map(|seed_range| ternary_search(seed_range.0, seed_range.1, |i| map_seed_to_location(i, &maps))).collect::<Vec<_>>();
+    // let fars = seed_ranges.iter().map(|seed_range| binary_search_minimizer(&maps, seed_range.0, seed_range.1)).collect::<Vec<_>>();
 
+    let low_bound = seed_ranges[0].0;
+    let high_bound = seed_ranges[0].1;
+
+    let result = binary_search_minimizer(&maps, low_bound, high_bound);
+
+    dbg!(seed_ranges);
+    println!("Part 2: {:?}", map_seed_to_location(79, &maps));
+    println!("Part 2: {:?}", map_seed_to_location(82, &maps));
+    dbg!(result);
+    // dbg!(fars);
 }
