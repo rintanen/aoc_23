b2f5b7e9e705ddca6dc1e50a787d26c514e68afa
rintanen <pee.rintanen@gmail.com>
2024-01-26 00:05:53 +0200

    adga

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use itertools::Itertools;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 enum Category {
     ExtremelyCoolLooking(u32),
     Musical(u32),
@@ -9,7 +9,21 @@ enum Category {
     Shiny(u32),
 }
 
-#[derive(Debug)]
+impl Category {
+    fn from_str(s: &str) -> Self {
+        let (category, value) = s.split_once("=").unwrap();
+        let value = value.parse::<u32>().unwrap();
+        match category {
+            "x" => Category::ExtremelyCoolLooking(value),
+            "m" => Category::Musical(value),
+            "a" => Category::Aerodynamical(value),
+            "s" => Category::Shiny(value),
+            _ => unreachable!()
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 enum Outcome {
     Accept,
     Reject,
@@ -26,7 +40,7 @@ impl Outcome {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Rule {
     category: Category,
     comp_symbol: char,
@@ -57,35 +71,50 @@ impl Rule {
             comp_symbol,
             outcome
         }
-
     }
-//
-//     fn test(&self, category: Category) -> Option<Outcome> {
-//         // if category is same enum variant as self.category compare the values
-//         // else return None
-//         if category == self.category {
-//             match self.comp_symbol {
-//                 '<' => {
-//                     if category < self.category {
-//                         Some(self.outcome)
-//                     } else {
-//                         None
-//                     }
-//                 },
-//                 '>' => {
-//                     if category > self.category {
-//                         Some(self.outcome)
-//                     } else {
-//                         None
-//                     }
-//                 },
-//         } else {
-//             None
-//         }
-//     }
+    fn test(&self, part: &Part) -> Option<Outcome> {
+        match self.category {
+            Category::ExtremelyCoolLooking(x) => {
+                if self.compare(part.cool, x) {
+                    Some(self.outcome.clone())
+                } else {
+                    None
+                }
+            },
+            Category::Musical(m) => {
+                if self.compare(part.musical, m) {
+                    Some(self.outcome.clone())
+                } else {
+                    None
+                }
+            },
+            Category::Aerodynamical(a) => {
+                if self.compare(part.aerodynamic, a) {
+                    Some(self.outcome.clone())
+                } else {
+                    None
+                }
+            },
+            Category::Shiny(s) => {
+                if self.compare(part.shiny, s) {
+                    Some(self.outcome.clone())
+                } else {
+                    None
+                }
+            },
+        }
+    }
+
+    fn compare(&self, a: u32, b: u32) -> bool {
+        match self.comp_symbol {
+            '<' => a < b,
+            '>' => a > b,
+            _ => unreachable!()
+        }
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Workflow {
     name: String,
     rules: Vec<Rule>,
@@ -122,11 +151,55 @@ fn parse_workflow(workflow: &str) -> Workflow {
 
 #[derive(Debug)]
 struct Part {
-    x: Category,
-    a: Category,
-    m: Category,
-    s: Category
+    cool: u32,
+    musical: u32,
+    aerodynamic: u32,
+    shiny: u32
 }
+
+
+
+fn process_part(workflows: &HashMap<String, Workflow>, part: &Part) -> Outcome {
+    // start by putting the part to 'in' workflow
+    let mut current_workflow = workflows.get("in").unwrap();
+    loop {
+        let mut final_outcome_determined = false;
+        for rule in current_workflow.rules.iter() {
+            match rule.test(part)  {
+                Some(Outcome::Accept) => {
+                    return Outcome::Accept;
+                },
+                Some(Outcome::Reject) => {
+                    return Outcome::Reject;
+                },
+                Some(Outcome::AnotherWorkflow(name)) => {
+                    current_workflow = workflows.get(&name).unwrap();
+                    final_outcome_determined = true;
+                    break;
+                },
+                None => {
+                    continue;
+                }
+            }
+        }
+
+        // only test finally if no rule matched
+        if !final_outcome_determined {
+            match &current_workflow.finally {
+                Outcome::Accept => {
+                    return Outcome::Accept;
+                },
+                Outcome::Reject => {
+                    return Outcome::Reject;
+                },
+                Outcome::AnotherWorkflow(name) => {
+                    current_workflow = workflows.get(name).unwrap();
+                }
+            }
+        }
+    }
+}
+
 
 fn main() {
     let input = include_str!("../../inputs/day19.in");
@@ -139,7 +212,7 @@ fn main() {
 
     let mut parts: Vec<Part> = Vec::new();
     for line in items.lines() {
-        let (x, a, m, s) = {
+        let (x, m, a, s) = {
             line[1..line.len() - 1]
                 .split(',')
                 .map(|s| {
@@ -150,14 +223,31 @@ fn main() {
                 .unwrap()
         };
         let part = Part {
-            x: Category::ExtremelyCoolLooking(x),
-            a: Category::Aerodynamical(a),
-            m: Category::Musical(m),
-            s: Category::Shiny(s)
+            cool: x,
+            musical: m,
+            aerodynamic: a,
+            shiny: s
         };
         parts.push(part);
     }
 
-    dbg!(workflows);
-    dbg!(parts);
+    let mut accepted: Vec<Part> = Vec::new();
+    let mut rejected: Vec<Part> = Vec::new();
+    for part in parts.into_iter() {
+        let outcome = process_part(&workflows, &part);
+        match outcome {
+            Outcome::Accept => {
+                accepted.push(part);
+            },
+            Outcome::Reject => {
+                rejected.push(part);
+            },
+            _ => unreachable!()
+        }
+    }
+
+    let sum_of_ratings: u32 = accepted.iter().map(|p| p.cool + p.musical + p.aerodynamic + p.shiny).sum();
+    dbg!(sum_of_ratings);
+
+
 }
